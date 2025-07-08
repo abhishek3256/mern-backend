5d7384fcece7c93a7240706a273666fdb0536d80
abhishek3256 <trexroy16@gmail.com>
2025-07-08 10:48:53 +0530

    yes

--- server.js
+++ server.js
@@ -5,18 +5,11 @@ import userRouter from "./routes/userRoute.js";
 dotenv.config();
 
 const app = express();
-
-// mongoose
-//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
-//   .then(() => {
-//     app.listen(8080, () => {
-//       console.log("Server started");
-//     });
-//   });
+const dbuser = process.env.DBUSER;
+const dbpass = process.env.DBPASS;
 
 mongoose
-  .connect(`mongodb+srv://penfighterveteran:6f0eK9qXWMTupH35@lpua.c2lnlun.mongodb.net/mernapp?retryWrites=true&w=majority
-`)
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@lpua.c2lnlun.mongodb.net/mernapp?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(8080, () => {
       console.log("Server started");
