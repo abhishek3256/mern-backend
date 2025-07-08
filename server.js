c394804c7ce5238b47652b64828d5c15b40108fa
abhishek3256 <trexroy16@gmail.com>
2025-07-08 10:40:05 +0530

    string

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 import express from "express";
-import userRouter from "./routes/userRoute.js"
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+dotenv.config();
+
 const app = express();
-mongoose.connect("mongodb://localhost:27017/lpu").then(() => {
-  app.listen(8080, () => {
-    console.log("Server started");
+
+// mongoose
+//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
+mongoose
+  .connect(`mongodb+srv://penfighterveteran:6f0eK9qXWMTupH35@lpua.c2lnlun.mongodb.net/mernapp?retryWrites=true&w=majority
+`)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
   });
-});
 app.use(express.json());
 app.use("/api/users", userRouter);
