9c42114bba276c7ce0066cc1c6f65717f1a4ba58
abhishek3256 <trexroy16@gmail.com>
2025-07-17 11:15:27 +0530

    yes

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-import mongoose from "mongoose";
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors";
 import userRouter from "./routes/userRoute.js";
-
+import productRouter from "./routes/productRoute.js";
+import orderRouter from "./routes/orderRoute.js"
+import cors from "cors";
 dotenv.config();
-
 const app = express();
 app.use(cors());
-const dbuser = process.env.DBUSER;
-const dbpass = process.env.DBPASS;
+app.use(express.json());
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+
 
 mongoose
-  .connect(`mongodb+srv://${dbuser}:${dbpass}@lpua.c2lnlun.mongodb.net/mernapp?retryWrites=true&w=majority`)
+  .connect(
+    `mongodb+srv://${dbuser}:${dbpass}@lpua.c2lnlun.mongodb.net/`
+  )
   .then(() => {
     app.listen(8080, () => {
       console.log("Server started");
     });
   });
-app.use(express.json());
+
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter)
